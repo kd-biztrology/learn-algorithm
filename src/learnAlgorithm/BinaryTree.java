410c1e5944fb713f22414581fead64027ac6b845
kevin duan <agonyice0115@gmail.com>
2015-12-15 14:33:48 +0800

    update

--- src/learnAlgorithm/BinaryTree.java
+++ src/learnAlgorithm/BinaryTree.java
@@ -1,3 +1,5 @@
+package learnAlgorithm;
+
 import java.util.LinkedList;
 
 /**
@@ -198,41 +200,4 @@ public class BinaryTree {
 
 }
 
-class Node<V>{
-    private V  value;
-    private Node<V> leftChild;
-    private Node<V> rightChild;
 
-    public Node(){
-    };
-
-    public Node(V value) {
-        this.value = value;
-        leftChild = null;
-        rightChild = null;
-    }
-
-    public void setLeftChild(Node<V> lNode) {
-        this.leftChild = lNode;
-    }
-
-    public void setRightChild(Node<V> rNode) {
-        this.rightChild = rNode;
-    }
-
-    public V getValue() {
-        return value;
-    }
-
-    public void setValue(V value) {
-        this.value = value;
-    }
-
-    public Node<V> getLeftChild() {
-        return leftChild;
-    }
-
-    public Node<V> getRightChild() {
-        return rightChild;
-    }
-}
---
+++ src/learnAlgorithm/Node.java
@@ -0,0 +1,45 @@
+package learnAlgorithm;
+
+/**
+ * Created by kevin.
+ */
+public class Node<V> {
+    private V value;
+    private Node<V> leftChild;
+    private Node<V> rightChild;
+
+    public Node() {
+    }
+
+    ;
+
+    public Node(V value) {
+        this.value = value;
+        leftChild = null;
+        rightChild = null;
+    }
+
+    public void setLeftChild(Node<V> lNode) {
+        this.leftChild = lNode;
+    }
+
+    public void setRightChild(Node<V> rNode) {
+        this.rightChild = rNode;
+    }
+
+    public V getValue() {
+        return value;
+    }
+
+    public void setValue(V value) {
+        this.value = value;
+    }
+
+    public Node<V> getLeftChild() {
+        return leftChild;
+    }
+
+    public Node<V> getRightChild() {
+        return rightChild;
+    }
+}
