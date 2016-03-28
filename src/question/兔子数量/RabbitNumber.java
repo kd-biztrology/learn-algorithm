783b86764c1d4699d0450bc6cc69c08de1f420f0
kevin duan <agonyice0115@gmail.com>
2016-03-28 14:25:42 +0800

    update code

--- src/question/兔子数量/RabbitNumber.java
+++ src/question/兔子数量/RabbitNumber.java
@@ -6,7 +6,7 @@ package question.兔子数量;
  * 题目：古典问题：有一对兔子，从出生后第3个月起每个月都生一对兔子，小兔子长到第三个月后每个月又生一对兔子，假如兔子都不死，问每个月的兔子总数为多少？
  * 程序分析： 兔子的规律为数列1,1,2,3,5,8,13,21....
  */
-public class TestOne {
+public class RabbitNumber {
     public static final int MONTH = 15;
     public static void main(String[] args) {
         long f1 = 1L, f2 = 1L;
