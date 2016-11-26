632239a68c346dbd356385d2b4a5e7140fe3755c
kevin duan <agonyice0115@gmail.com>
2016-11-26 17:00:58 +0800

    update

--- src/question/二叉树/BinaryTree.java
+++ src/question/二叉树/BinaryTree.java
@@ -3,7 +3,8 @@ package question.二叉树;
 import java.util.LinkedList;
 
 /**
- * Created by kevin . 通过数组构造二叉树，所有遍历算法以及求二叉树深度的递归算法
+ * 通过数组构造二叉树，所有遍历算法以及求二叉树深度的递归算法
+ * Created by kevin .
  */
 public class BinaryTree {
   //根节点
@@ -48,184 +49,188 @@ public class BinaryTree {
     }
   }
 
-	public static void main(String[] args) {
-		BinaryTree bt = new BinaryTree(new int[]{1, 2, 3, 4, 5, 6, 7, 8});
-		bt.inorder();
-		bt.preorder();
-		bt.layerorder();
-		bt.preorderNoRecursion();
-		bt.inorderNoRecursion();
-		bt.postorderNoRecursion();
-		System.out.println("深度为：" + bt.getDepth());
-	}
+  public static void main(String[] args) {
+    BinaryTree bt = new BinaryTree(new int[] {1, 2, 3, 4, 5, 6, 7, 8});
+    bt.inorder();
+    bt.preorder();
+    bt.layerorder();
+    bt.preorderNoRecursion();
+    bt.inorderNoRecursion();
+    bt.postorderNoRecursion();
+    System.out.println("深度为：" + bt.getDepth());
+  }
 
+  //递归中序遍历
+  private void inorder() {
+    System.out.print("binaryTree递归中序遍历:");
+    inorderTraverseRecursion(root);
+    System.out.println();
+  }
 
-	//递归中序遍历
-	private void inorder() {
-		System.out.print("binaryTree递归中序遍历:");
-		inorderTraverseRecursion(root);
-		System.out.println();
-	}
+  //层次遍历
+  private void layerorder() {
+    System.out.print("binaryTree层次遍历:");
+    LinkedList<Node<Integer>> queue = new LinkedList<>();
+    queue.addLast(root);
+    Node<Integer> current;
+    while (!queue.isEmpty()) {
+      current = queue.removeFirst();
+      if (current.getLeftChild() != null) {
+        queue.addLast(current.getLeftChild());
+      }
+      if (current.getRightChild() != null) {
+        queue.addLast(current.getRightChild());
+      }
+      System.out.print(current.getValue());
+    }
+    System.out.println();
+  }
 
-	//层次遍历
-	private void layerorder() {
-		System.out.print("binaryTree层次遍历:");
-		LinkedList<Node<Integer>> queue = new LinkedList<>();
-		queue.addLast(root);
-		Node<Integer> current;
-		while (!queue.isEmpty()) {
-			current = queue.removeFirst();
-			if (current.getLeftChild() != null)
-				queue.addLast(current.getLeftChild());
-			if (current.getRightChild() != null)
-				queue.addLast(current.getRightChild());
-			System.out.print(current.getValue());
-		}
-		System.out.println();
-	}
+  //获得二叉树深度
+  private int getDepth() {
+    return getDepthRecursion(root);
+  }
 
+  private int getDepthRecursion(Node<Integer> node) {
+    if (node == null) {
+      return 0;
+    }
+    int llen = getDepthRecursion(node.getLeftChild());
+    int rlen = getDepthRecursion(node.getRightChild());
+    int maxlen = Math.max(llen, rlen);
+    return maxlen + 1;
+  }
 
-	//获得二叉树深度
-	private int getDepth() {
-		return getDepthRecursion(root);
-	}
+  //递归先序遍历
+  private void preorder() {
+    System.out.print("binaryTree递归先序遍历:");
+    preorderTraverseRecursion(root);
+    System.out.println();
+  }
 
-	private int getDepthRecursion(Node<Integer> node) {
-		if (node == null)
-			return 0;
-		int llen = getDepthRecursion(node.getLeftChild());
-		int rlen = getDepthRecursion(node.getRightChild());
-		int maxlen = Math.max(llen, rlen);
-		return maxlen + 1;
-	}
+  private void inorderTraverseRecursion(Node<Integer> node) {
+    // TODO Auto-generated method stub
+    if (node.getLeftChild() != null) {
+      inorderTraverseRecursion(node.getLeftChild());
+    }
+    System.out.print(node.getValue());
+    if (node.getRightChild() != null) {
+      inorderTraverseRecursion(node.getRightChild());
+    }
+  }
 
-	//递归先序遍历
-	private void preorder() {
-		System.out.print("binaryTree递归先序遍历:");
-		preorderTraverseRecursion(root);
-		System.out.println();
-	}
+  private void preorderTraverseRecursion(Node<Integer> node) {
+    System.out.print(node.getValue());
+    if (node.getLeftChild() != null) {
+      preorderTraverseRecursion(node.getLeftChild());
+    }
+    if (node.getRightChild() != null) {
+      preorderTraverseRecursion(node.getRightChild());
+    }
+  }
 
+  //非递归先序遍历
+  private void preorderNoRecursion() {
+    System.out.print("binaryTree非递归先序遍历:");
+    LinkedList<Node<Integer>> stack = new LinkedList<>();
+    stack.push(root);
+    Node<Integer> current;
+    while (!stack.isEmpty()) {
+      current = stack.pop();
+      System.out.print(current.getValue());
+      if (current.getRightChild() != null) {
+        stack.push(current.getRightChild());
+      }
+      if (current.getLeftChild() != null) {
+        stack.push(current.getLeftChild());
+      }
+    }
+    System.out.println();
+  }
 
-	private void inorderTraverseRecursion(Node<Integer> node) {
-		// TODO Auto-generated method stub
-		if (node.getLeftChild() != null)
-			inorderTraverseRecursion(node.getLeftChild());
-		System.out.print(node.getValue());
-		if (node.getRightChild() != null)
-			inorderTraverseRecursion(node.getRightChild());
-	}
+  /**
+   * 非递归中序遍历 栈内保存将要访问的元素
+   */
+  private void inorderNoRecursion() {
+    System.out.print("binaryTree非递归中序遍历:");
+    LinkedList<Node<Integer>> stack = new LinkedList<>();
+    Node<Integer> current = root;
+    while (current != null || !stack.isEmpty()) {
+      while (current != null) {
+        stack.push(current);
+        current = current.getLeftChild();
+      }
+      if (!stack.isEmpty()) {
+        current = stack.pop();
+        System.out.print(current.getValue());
+        current = current.getRightChild();
+      }
+    }
+    System.out.println();
+  }
 
+  /**
+   * 非递归后序遍历
+   * <p>
+   * 当上一个访问的结点是右孩子或者当前结点没有右孩子则访问当前结点
+   */
+  private void postorderNoRecursion() {
+    System.out.print("binaryTree非递归后序遍历:");
+    Node<Integer> rNode = null;
+    Node<Integer> current = root;
+    LinkedList<Node<Integer>> stack = new LinkedList<>();
+    while (current != null || !stack.isEmpty()) {
+      while (current != null) {
+        stack.push(current);
+        current = current.getLeftChild();
+      }
+      current = stack.pop();
+      while (current != null && (current.getRightChild() == null
+          || current.getRightChild() == rNode)) {
+        System.out.print(current.getValue());
+        rNode = current;
+        if (stack.isEmpty()) {
+          System.out.println();
+          return;
+        }
+        current = stack.pop();
+      }
+      stack.push(current);
+      current = current != null ? current.getRightChild() : null;
+    }
+  }
 
-	private void preorderTraverseRecursion(Node<Integer> node) {
-		System.out.print(node.getValue());
-		if (node.getLeftChild() != null)
-			preorderTraverseRecursion(node.getLeftChild());
-		if (node.getRightChild() != null)
-			preorderTraverseRecursion(node.getRightChild());
-	}
+  class Node<V> {
+    private V value;
+    private Node<V> leftChild;
+    private Node<V> rightChild;
 
-	//非递归先序遍历
-	private void preorderNoRecursion() {
-		System.out.print("binaryTree非递归先序遍历:");
-		LinkedList<Node<Integer>> stack = new LinkedList<>();
-		stack.push(root);
-		Node<Integer> current;
-		while (!stack.isEmpty()) {
-			current = stack.pop();
-			System.out.print(current.getValue());
-			if (current.getRightChild() != null)
-				stack.push(current.getRightChild());
-			if (current.getLeftChild() != null)
-				stack.push(current.getLeftChild());
-		}
-		System.out.println();
-	}
+    Node(V value) {
+      this.value = value;
+      leftChild = null;
+      rightChild = null;
+    }
 
-	/**
-	 * 非递归中序遍历 栈内保存将要访问的元素
-	 */
-	private void inorderNoRecursion() {
-		System.out.print("binaryTree非递归中序遍历:");
-		LinkedList<Node<Integer>> stack = new LinkedList<>();
-		Node<Integer> current = root;
-		while (current != null || !stack.isEmpty()) {
-			while (current != null) {
-				stack.push(current);
-				current = current.getLeftChild();
-			}
-			if (!stack.isEmpty()) {
-				current = stack.pop();
-				System.out.print(current.getValue());
-				current = current.getRightChild();
-			}
-		}
-		System.out.println();
-	}
+    void setLeftChild(Node<V> lNode) {
+      this.leftChild = lNode;
+    }
 
-	/**
-	 * 非递归后序遍历
-	 *
-	 * 当上一个访问的结点是右孩子或者当前结点没有右孩子则访问当前结点
-	 */
-	private void postorderNoRecursion() {
-		System.out.print("binaryTree非递归后序遍历:");
-		Node<Integer> rNode = null;
-		Node<Integer> current = root;
-		LinkedList<Node<Integer>> stack = new LinkedList<>();
-		while (current != null || !stack.isEmpty()) {
-			while (current != null) {
-				stack.push(current);
-				current = current.getLeftChild();
-			}
-			current = stack.pop();
-			while (current != null && (current.getRightChild() == null || current.getRightChild() == rNode)) {
-				System.out.print(current.getValue());
-				rNode = current;
-				if (stack.isEmpty()) {
-					System.out.println();
-					return;
-				}
-				current = stack.pop();
-			}
-			stack.push(current);
-			current = current != null ? current.getRightChild() : null;
-		}
+    void setRightChild(Node<V> rNode) {
+      this.rightChild = rNode;
+    }
 
-	}
+    V getValue() {
+      return value;
+    }
 
-	class Node<V> {
-		private V value;
-		private Node<V> leftChild;
-		private Node<V> rightChild;
+    Node<V> getLeftChild() {
+      return leftChild;
+    }
 
-		Node(V value) {
-			this.value = value;
-			leftChild = null;
-			rightChild = null;
-		}
-
-		void setLeftChild(Node<V> lNode) {
-			this.leftChild = lNode;
-		}
-
-		void setRightChild(Node<V> rNode) {
-			this.rightChild = rNode;
-		}
-
-		V getValue() {
-			return value;
-		}
-
-		Node<V> getLeftChild() {
-			return leftChild;
-		}
-
-		Node<V> getRightChild() {
-			return rightChild;
-		}
-	}
-
+    Node<V> getRightChild() {
+      return rightChild;
+    }
+  }
 }
 
 
